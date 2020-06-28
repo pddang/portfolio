32bad0c008bbc86798f30ac8812e0f2da7d7971d
Phan Dang <philip.ddang>
2020-06-28 12:30:34 -0700

    frontend and backend updated

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1>This webpage will be transformed</h1>
+    </div>
+  );
+}
+
+export default App;
